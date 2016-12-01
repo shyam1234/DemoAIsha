474883e41fc77df7b135b0e3202e35beb48bfe83
malviya <malviya.software@gmail.com>
2016-12-01 19:06:46 +0530

    update

--- app/src/main/java/com/malviya/demoaisha/MainActivity.java
+++ app/src/main/java/com/malviya/demoaisha/MainActivity.java
@@ -2,15 +2,29 @@ package com.malviya.demoaisha;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 /**
  * Created by 23508 on 11/30/2016.
  */
 
 public class MainActivity extends Activity {
+    private static final int SPEECH_REQUEST_CODE = 1;
+    private static final String TAG = "ITC";
+    private SpeechRecognizer speechRecognizer;
+    private Intent recognizerIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/malviya/demoaisha/MainService.java
+++ app/src/main/java/com/malviya/demoaisha/MainService.java
@@ -1,19 +1,36 @@
 package com.malviya.demoaisha;
 
+import android.app.Activity;
 import android.app.Service;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 /**
  * Created by 23508 on 11/30/2016.
@@ -22,13 +39,17 @@ import java.util.TimerTask;
 public class MainService extends Service {
 
     private TextToSpeech mTextToSpeech;
+    private static final int SPEECH_REQUEST_CODE = 0;
+    private Handler mHandler;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
 
 
     @Override
     public void onCreate() {
         super.onCreate();
         Toast.makeText(MainService.this, "Service Started", Toast.LENGTH_SHORT).show();
-
+        //registerReceiver(new MyReceiver(),new IntentFilter(RecognizerIntent.ACTION_RECOGNIZE_SPEECH));
 
     }
 
@@ -52,6 +73,14 @@ public class MainService extends Service {
 
     private void init() {
 
+        speech = SpeechRecognizer.createSpeechRecognizer(MainService.this);
+        speech.setRecognitionListener(new Listner());
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getApplicationContext().getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+
 
         mTextToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -63,18 +92,32 @@ public class MainService extends Service {
                     timer.schedule(new TimerTask() {
                         @Override
                         public void run() {
-                            mTextToSpeech.speak("Hello Maalviya. How are you? Now, Time is  "
-                                    + Calendar.getInstance().get(Calendar.HOUR)
-                                    + " : " + Calendar.getInstance().get(Calendar.MINUTE)
-                                    + "Thank you.", TextToSpeech.QUEUE_FLUSH, null);
+//                            mTextToSpeech.speak("Hello Maalviya. How are you? Now, Time is  "
+//                                    + Calendar.getInstance().get(Calendar.HOUR)
+//                                    + " : " + Calendar.getInstance().get(Calendar.MINUTE)
+//                                    + "Thank you.", TextToSpeech.QUEUE_FLUSH, null);
+                            displaySpeechRecognizer();
 
-                            //Log.d("ITC","time "+Calendar.getInstance().get(Calendar.HOUR)+":"+Calendar.getInstance().get(Calendar.MINUTE));
                         }
-                    }, 5000, 1000 * 60 * 60);
+                    }, 5000, 20000/*1000 * 60 * 60*/);
 
                 }
             }
         });
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message) {
+                speech.startListening(recognizerIntent);
+            }
+        };
+    }
+
+
+
+
+    private void displaySpeechRecognizer() {
+        mHandler.sendEmptyMessage(0);
     }
 
 
@@ -84,7 +127,95 @@ public class MainService extends Service {
             mTextToSpeech.stop();
             mTextToSpeech.shutdown();
         }
+
+        if (speech != null) {
+            speech.stopListening();
+            speech.destroy();
+            Log.i("ITC", "destroy");
+        }
         super.onDestroy();
     }
+
+
+    private class Listner implements RecognitionListener {
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.i("ITC", "onEndOfSpeech ");
+            speech.stopListening();
+        }
+
+        @Override
+        public void onError(int errorCode) {
+            Log.i("ITC", "results " + errorCode);
+            String errorMessage = VoiceRecognitionActivity.getErrorText(errorCode);
+            Log.i("ITC", "results " + errorMessage);
+            speech.stopListening();
+        }
+
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+            Log.i("ITC", "results " + i);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            Log.i("ITC", "results " + results);
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String text = matches.get(0).toString();
+            for (String result : matches) {
+                text += result + "\n";
+                Log.i("ITC", "results " + result);
+            }
+            Log.d("ITC", "final text: " + text);
+
+
+            rectAIsha(text);
+
+        }
+    }
+
+    private void rectAIsha(String text) {
+        if (text.contains("time")){
+            mTextToSpeech.speak("Time is  "
+                                    + Calendar.getInstance().get(Calendar.HOUR)
+                                    + " : " + Calendar.getInstance().get(Calendar.MINUTE)
+                                    + "Thank you.", TextToSpeech.QUEUE_FLUSH, null);
+        }else if (text.contains("how are you")){
+            mTextToSpeech.speak("Hello Maalviya, I am good. How are you?", TextToSpeech.QUEUE_FLUSH, null);
+        }else if(text.contains("plan")){
+            mTextToSpeech.speak("I do not have any special plan today. You say", TextToSpeech.QUEUE_FLUSH, null);
+        }else{
+            mTextToSpeech.speak("Sorry, I did not get you", TextToSpeech.QUEUE_FLUSH, null);
+        }
+
+    }
+
 
 }
